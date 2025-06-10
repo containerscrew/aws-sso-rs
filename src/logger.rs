fd34df78d415cfef22a5024a9b692f993d3ed664
containerscrew <containerscrew@users.noreply.github.com>
2025-06-11 00:05:11 +0200

    refactor(logger): logger format

--- src/logger.rs
+++ src/logger.rs
@@ -5,11 +5,12 @@ pub fn setup_logger(log_level: &String) {
         "info" => tracing::Level::INFO,
         "warn" => tracing::Level::WARN,
         "error" => tracing::Level::ERROR,
-        _ => tracing::Level::ERROR,
+        _ => tracing::Level::INFO,
     };
 
     tracing_subscriber::fmt()
-        .with_thread_names(true)
+        .with_thread_names(false)
+        .with_target(false)
         .with_max_level(log_level)
         .without_time()
         .init();
--- src/macros.rs
+++
@@ -1,8 +0,0 @@
-// Macro print orange
-
-#[macro_export]
-macro_rules! println_orange {
-    ($($arg:tt)*) => {{
-        println!("[aws-sso-rs] - {}", format!($($arg)*).truecolor(255, 165, 0));
-    }};
-}
